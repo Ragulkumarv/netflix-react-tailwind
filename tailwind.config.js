19913b0a69207983a2dc7ab68a33d8f2a43f774f
Ragul <ragulkumarv333@gmail.com>
2024-08-09 14:19:53 +0530

    added gpt btn & animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       screens: {
         custom: "885px", // custom breakpoint for padding
       },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
     },
   },
   plugins: [
